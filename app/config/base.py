8f6e24f672721511e19ecd2ec452c6222efed7fc
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2023-10-30 15:19:53 -0300

    chore: add base config for cache

--- app/config/base.py
+++ app/config/base.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from . import getenv_or_action
+
 # Password hashing configuration
 PASSWORD_HASH_ALGORITHM = "pbkdf2_sha256"
 PASSWORD_HASH_NUMBER_OF_ITERATIONS = 60000
@@ -10,3 +12,11 @@ TIMEZONE = "America/Sao_Paulo"
 SENTRY_ENABLE = False
 SENTRY_DSN = None
 SENTRY_ENVIRONMENT = None
+
+# Cache
+CACHE_ENABLE = getenv_or_action("CACHE_ENABLE", default="false").lower() == "true"
+if CACHE_ENABLE:
+    CACHE_REDIS_URL = getenv_or_action("CACHE_REDIS_URL", action="raise")
+else:
+    CACHE_REDIS_URL = None
+CACHE_DEFAULT_TIMEOUT = int(getenv_or_action("CACHE_DEFAULT_TIMEOUT", default="43200"))  # 12 hours
